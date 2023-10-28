4b655129cebf21c7adff263f7e9be70f88dab3bb
Blackbeing <johnjahi55@gmail.com>
2023-10-28 15:01:21 +0300

    feat Get stats of objects using API

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,6 +6,7 @@ that returns a JSON: "status": "OK" """
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route("/status", methods=["GET"])
@@ -13,3 +14,21 @@ def api_status():
     """returns JSON status"""
     response = {"status": "OK"}
     return jsonify(response)
+
+
+@app_views.route("/stats", methods=["GET"])
+def api_stats():
+    """Get api object stats"""
+    from models.amenity import Amenity
+    from models.city import City
+    from models.place import Place
+    from models.review import Review
+    from models.state import State
+    from models.user import User
+
+    classes = [Amenity, City, Place, Review, State, User]
+    # for klass in classes:
+    #     objects_count[klass.__qualname__] = storage.count(klass)
+    # return objects_count
+    return {klass.__qualname__.lower():
+            storage.count(klass) for klass in classes}
